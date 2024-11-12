a302bd8636e03d2cf3cc62fbfb0e10e5323c61b5
hedensilva <38959643+hedensilva@users.noreply.github.com>
2024-11-12 14:13:55 -0300

    Add files via upload

--- escola.sql
+++ escola.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/11/2024 às 18:06
+-- Tempo de geração: 12/11/2024 às 18:13
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -34,6 +34,39 @@ CREATE TABLE `aluno` (
   `transporte_escolar` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `aluno_esporte`
+--
+
+CREATE TABLE `aluno_esporte` (
+  `id` int(11) NOT NULL,
+  `ano` varchar(20) NOT NULL,
+  `id_aluno` int(11) NOT NULL,
+  `id_esporte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `esporte`
+--
+
+CREATE TABLE `esporte` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `esporte`
+--
+
+INSERT INTO `esporte` (`id`, `nome`) VALUES
+(1, 'Futsal'),
+(2, 'Vôlei'),
+(3, 'Basquete');
+
 --
 -- Índices para tabelas despejadas
 --
@@ -43,6 +76,22 @@ CREATE TABLE `aluno` (
 --
 ALTER TABLE `aluno`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `esporte`
+--
+ALTER TABLE `esporte`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `esporte`
+--
+ALTER TABLE `esporte`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
